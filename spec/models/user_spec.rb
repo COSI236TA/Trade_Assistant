b01295ee4ef4350502e4e2d683d04dda4c20aa81
EdTheMan <chiieong@brandeis.edu>
2014-03-13 21:25:53 -0400

    Using RSpec, error in dashboard checking..

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,13 @@ require '..\spec_helper'
 
 describe User do
 
-  before { @user = User.new(email: "user@example.com") }
+  before { @user = User.new(:email => 'test@example.com',:password => 'a',:password_confirmation=> 'a') }
 
   subject { @user }
 
   it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_digest) }
+
+
 end
