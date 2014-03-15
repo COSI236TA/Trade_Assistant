7fbb8be455801248e420b8f75c19707850f0b525
EdTheMan <chiieong@brandeis.edu>
2014-03-15 01:38:49 -0400

    Before moving to vagrant

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rspec-rails'
+gem 'capybara'
 #gem "spring", group: :development
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
---
+++ spec/page_spec/user_pages_spec.rb
@@ -0,0 +1,33 @@
+require '../spec_helper'
+
+
+describe "User pages" do
+
+  subject { page }
+
+  describe "register" do
+
+    before { visit register_path }
+
+    let(:submit) { "Create User" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+
+      before do
+        fill_in "Email",        with: "user2@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Password confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
+end
