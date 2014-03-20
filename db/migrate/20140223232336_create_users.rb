c24a0ed3bf29a2afe6762d495a4eda9995e55fdd
Noranda Brown <noranda@norandabrown.com>
2014-03-20 18:10:12 -0400

    Reset the wrong file

--- db/migrate/20140223232336_create_users.rb
+++ db/migrate/20140223232336_create_users.rb
@@ -1,9 +1,10 @@
-class UserModel < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration
   def change
     drop_table :users
     create_table :users do |t|
+      #t.string :name
       t.string :email
-      t.string :password_digest
+      t.string :password_digest 
       t.timestamps
     end
   end
