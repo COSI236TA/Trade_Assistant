f6e3855f7ec095603f31e5d6939d5ed9ab154499
Wenbin Xiao <xwb1989@gmail.com>
2014-03-20 14:51:03 -0400

    modify session control

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,25 +1,30 @@
 module SessionsHelper
 
   def go_to_dashboard
-      format.html { redirect_to dashboard_path }
+    format.html { redirect_to dashboard_path }
   end
 
 
   def get_email
-      #User.find_by(name: params[:name])
-      if !session[:user_id].nil? 
-      User.find(session[:user_id]).email
+    #User.find_by(name: params[:name])
+    if !session[:user_id].nil?
+      user = User.find(session[:user_id])
+      if user != nil
+        user.email
       else
-      "error"
+        "Error"
       end
+    else
+      "error"
+    end
   end
 
   def user_logged_in?
-      return (session[:user_id] != nil)
+    return (session[:user_id] != nil) && User.find(session[:user_id]) != nil
   end
-  
+
   def destroy
-      session[:user_id] = nil
+    session[:user_id] = nil
   end
 
 end
