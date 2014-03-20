03de7b5fa84d6fb2588dc0cb95f0c2f1b594db24
Noranda Brown <noranda@norandabrown.com>
2014-03-20 18:09:19 -0400

    Remove files I added by accident

--- db/migrate/20140223232336_create_users.rb
+++ db/migrate/20140223232336_create_users.rb
@@ -1,9 +1,9 @@
-class CreateUsers < ActiveRecord::Migration
+class UserModel < ActiveRecord::Migration
   def change
+    drop_table :users
     create_table :users do |t|
-      #t.string :name
       t.string :email
-      t.string :password_digest 
+      t.string :password_digest
       t.timestamps
     end
   end
