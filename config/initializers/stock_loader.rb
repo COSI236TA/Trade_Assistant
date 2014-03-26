53320739f44d081fa48098cb00d12dd84a3ce920
Wenbin Xiao <xwb1989@gmail.com>
2014-03-26 00:43:12 -0400

    add a initializer --- The initializer will yield a global constant STOCK_LIST containing the whole list of stock's names and symbols

---
+++ config/initializers/stock_loader.rb
@@ -0,0 +1,17 @@
+#load stock symbols and name from txt file to memory
+
+
+def load_stocks
+  require 'csv'
+  path = "completelist"
+  file = "#{Rails.root}/app/txtfiles/#{path}"
+
+  stock_list = []
+  CSV.foreach(file, :col_sep => "|") do |row|
+    entry = "#{row[1]}(#{row[0]})"
+    stock_list << entry
+  end
+  return stock_list
+end
+
+STOCK_LIST = load_stocks
