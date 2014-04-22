4380ca1ffec68542b609b84273888b8c3f713dd4
Wenbin Xiao <xwb1989@gmail.com>
2014-04-22 14:30:35 -0400

    move get_porfolio.htm.erb to views of portfolio and move the corresponding action to portfolios' controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -78,6 +78,45 @@ class PortfoliosController< ApplicationController
     end
   end
 
+  #get portfolio page and display it in iframe
+  #lightbox portfolio page
+  def get_portfolio
+     #gets portfolio id
+     portfolio = Portfolio.find(params[:portfolio_id])
+
+     #portfolio information
+     @portfolio_name = portfolio.name.gsub(/\b\w/){ $&.upcase } #Makes each first char upper case
+     @portfolio_description = portfolio.description
+     @portfolio_stocks = portfolio.stocks
+     @portfolio_rules = portfolio.rules
+
+     #store each ticker into an array
+     @stock_ticker_array = Array.new
+     @portfolio_stocks.each do |stock|
+
+     @stock_ticker_array << stock.ticker
+
+     end
+
+
+     client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = "07MR13DREG5LnUuiFqgXJTTDl"
+      config.consumer_secret     = "p3tR0GXc9o0AKWEwOPHWmV5wedlaIv6iNx7UBcg4ZzYP0KcmaQ"
+      config.access_token        = "2244833450-j7q6dhN5H3I3YNs5fM4VTlja1r31FEXCO3KpxHg"
+      config.access_token_secret = "V0GMof1aKZPKAUQzIErTfQtLyLpBATvf5h1Ah158CGlB9"
+     end
+
+      p "********************************"
+
+      p client.search("GOOGLE", :count => 100).first.text
+      p client.search("GOOGLE", :count => 100).first.id
+  
+    if params[:type] == "iframe" 
+      render :layout=> "iframe_portfolio"
+    elsif params[:type] == "page"
+      render :layout => "application"
+    end
+  end
 
   # DELETE /portfolios/1
   # DELETE /portfolios/1.json
