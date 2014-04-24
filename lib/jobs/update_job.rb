673a452f295db33deb7df78f07c5449c1e657739
Wenbin Xiao <xwb1989@gmail.com>
2014-04-24 15:53:31 -0400

    update_job.rb changed to better structure

--- lib/jobs/update_job.rb
+++ lib/jobs/update_job.rb
@@ -4,12 +4,13 @@ class Jobs::UpdateJob
   def perform(data)
     puts "#{ Time.now }: Rule Engine starts updating stocks data and rule status" 
     RuleEngine::RuleEngine.start
+    Jobs::UpdateJob.new.async.later(300, "Update starts")
+
     puts "#{ Time.now }: Rule Engine finishes." 
   end
   def later(sec, data)
     after(sec) { 
       perform(data) 
-      later(sec, data) 
     }
   end
 end
