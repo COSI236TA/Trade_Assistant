901943cc5556556d9c8b5ebc52228d1f15cf37cf
Wenbin Xiao <xwb1989@gmail.com>
2014-05-02 18:07:04 -0400

    refractor the rule controller; extract argument preparation to rule helper

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -37,60 +37,62 @@ class RulesController < ApplicationController
   # POST /rules.json
   def create
     #Find the user
-    @user = User.find(session[:user_id])
+    # @user = User.find(session[:user_id])
 
-    #Get clean params
-    rule_clean_params = rule_params
+    # #Get clean params
+    # rule_clean_params = rule_params
 
-    #Get property
-    property = rule_clean_params[:property].to_s
+    # #Get property
+    # property = rule_clean_params[:property].to_s
 
-    #Get rel
-    rel = rule_clean_params[:rel]
+    # #Get rel
+    # rel = rule_clean_params[:rel]
 
-    #Get target
-    target = rule_clean_params[:target]
+    # #Get target
+    # target = rule_clean_params[:target]
 
-    #Get portfolio params
-    portfolio_params = params[:portfolio]
-    portfolio = nil
+    # #Get portfolio params
+    # portfolio_params = params[:portfolio]
+    # portfolio = nil
 
 
-    #deal with create or select portfolio
-    if portfolio_params[:action] == "select"
-      #need to create the portfolio
-      portfolio_id = portfolio_params[:id].to_i
-      portfolio = @user.portfolios.find(portfolio_id)
-    elsif portfolio_params[:action] == "create"
-      #get the tickers from params
-      tickers = portfolio_params[:ticker].values.map { |i| i[:ticker] }
-      name = portfolio_params[:name]
-      description = portfolio_params[:description]
+    # #deal with create or select portfolio
+    # if portfolio_params[:action] == "select"
+    #   #need to create the portfolio
+    #   portfolio_id = portfolio_params[:id].to_i
+    #   portfolio = @user.portfolios.find(portfolio_id)
+    # elsif portfolio_params[:action] == "create"
+    #   #get the tickers from params
+    #   tickers = portfolio_params[:ticker].values.map { |i| i[:ticker] }
+    #   name = portfolio_params[:name]
+    #   description = portfolio_params[:description]
 
-      portfolio = @user.portfolios.create(name: name, description: description)
+    #   portfolio = @user.portfolios.create(name: name, description: description)
 
-      tickers.each do |ticker|
-        DataPool::DataUpdater.update ticker
-        stock = Stock.find_by(ticker: ticker)
-        portfolio.stocks << stock
+    #   tickers.each do |ticker|
+    #     DataPool::DataUpdater.update ticker
+    #     stock = Stock.find_by(ticker: ticker)
+    #     portfolio.stocks << stock
 
-        puts portfolio.stocks
-      end
-    end
+    #     puts portfolio.stocks
+    #   end
+    # end
 
-    #create rule
+    # #create rule
 
-    #Build params for create rule
-    real_property = Property.find(property)
-    build_params = { 
-      :portfolio => portfolio,
-      :property => real_property,
-      :rel => rel,
-      :target => target, 
-      :name => "#{portfolio.name} #{portfolio.rules.size+1}",
-      :description => Rule.get_description(portfolio, real_property, rel, target)
-    }
-
+    # #Build params for create rule
+    # real_property = Property.find(property)
+    # build_params = { 
+    #   :portfolio => portfolio,
+    #   :property => real_property,
+    #   :rel => rel,
+    #   :target => target, 
+    #   :name => "#{portfolio.name} #{portfolio.rules.size+1}",
+    #   :description => Rule.get_description(portfolio, real_property, rel, target)
+    # }
+    
+    # prepare_parameters is in RuleHelper
+    build_params = prepare_parameters
     @rule = @user.rules.create(build_params)
     response = true
     if !@rule.valid?
